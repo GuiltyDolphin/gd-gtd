f90c16b508ccbd56870031380c787fd8c6291a16
Ben Moon <software@guiltydolphin.com>
2018-04-12 16:50:16 +0100

    Update package to use UMD build result

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 .PHONY: gd.js
 gd.js :
-	browserify src/gd.js -o gd.js
+	browserify src/gd.js --standalone gd -o gd.js
--- src/gd.js
+++ src/gd.js
@@ -1,13 +1,11 @@
 /** Library entry point
  *
- * gd.backend - interaction with backend data
- * gd.context - support for context inference
- * gd.review  - support for reviewing entries
+ * backend - interaction with backend data
+ * context - support for context inference
+ * review  - support for reviewing entries
  */
-var gd = gd || {};
-
-Object.assign(gd, {
+module.exports = {
     backend: require('./backend.js'),
     context: require('./context.js'),
     review : require('./review.js')
-});
+};
